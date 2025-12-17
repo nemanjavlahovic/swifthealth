0fa17cbc0fa4f285e5b1a6b3dccfd378398daff5
Nemanja Vlahović <nemanja@nemanjavlahovic.com>
2025-12-17 23:45:07 +0100

    chore: update formula SHA256 and Xcode requirement

--- homebrew/swifthealth.rb
+++ homebrew/swifthealth.rb
@@ -2,11 +2,11 @@ class Swifthealth < Formula
   desc "CLI tool that analyzes Swift project health and generates actionable reports"
   homepage "https://github.com/nemanjavlahovic/swifthealth"
   url "https://github.com/nemanjavlahovic/swifthealth/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "5c069680e778c649a58814f2ab158651db23b3c94396caf561ba99087d9bb60b"
+  sha256 "a5d2f9cf73f2829f17574cd93bcca6efa168a6495649d41c96c2caac93411720"
   license "MIT"
   head "https://github.com/nemanjavlahovic/swifthealth.git", branch: "main"
 
-  depends_on xcode: ["15.0", :build]
+  depends_on xcode: ["16.0", :build]
   depends_on :macos
 
   def install
