0ab0f6c850a9791d920d11bbe793f51dff8a0c2b
Nemanja Vlahović <nemanja@nemanjavlahovic.com>
2025-12-17 23:31:51 +0100

    chore: update formula SHA256 for v0.1.0

--- homebrew/swifthealth.rb
+++ homebrew/swifthealth.rb
@@ -2,7 +2,7 @@ class Swifthealth < Formula
   desc "CLI tool that analyzes Swift project health and generates actionable reports"
   homepage "https://github.com/nemanjavlahovic/swifthealth"
   url "https://github.com/nemanjavlahovic/swifthealth/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "5c069680e778c649a58814f2ab158651db23b3c94396caf561ba99087d9bb60b"
   license "MIT"
   head "https://github.com/nemanjavlahovic/swifthealth.git", branch: "main"
 
